4882e138e797995254409e813c92c4f45c58b63f
Francesco Lorenti <francesco1.lorenti@mail.polimi.it>
2017-05-09 18:48:45 +0200

    cards (early commit)

---
+++ src/main/java/it/polimi/ingsw/ps09/res/Cards/Character.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.ps09.res.Cards;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class Character {
+}
---
+++ src/main/java/it/polimi/ingsw/ps09/res/Cards/Venture.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.ps09.res.Cards;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class Venture {
+
+}
