c53686ca6875a6b4951b9ce8d213cd18b033fd77
francesco995 <francesco.gianni@live.it>
2017-05-09 11:21:09 +0200

    create client and server packages

---
+++ src/main/java/it/polimi/ingsw/ps09/server/App.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ps09.server;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello !" );
+    }
+}
