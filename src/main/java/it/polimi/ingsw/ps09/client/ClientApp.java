77b583bc3d3da6b59a3996cb9e3eeb732a8423d8
francesco995 <francesco.gianni@live.it>
2017-05-09 18:06:42 +0200

    Coins.java

--- src/main/java/it/polimi/ingsw/ps09/client/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ps09/client/ClientApp.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw.ps09.client;
 
+
 /**
  * Created by franc on 09/05/2017.
  */
 public class ClientApp {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Client!" );
+    public static void main(String[] args) {
+        System.out.println("Hello Client!");
     }
 }
---
+++ src/main/java/it/polimi/ingsw/ps09/res/Resources/Coins.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ps09.res.Resources;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class Coins {
+    int mValue = 0;
+
+    public int getValue() {
+        return mValue;
+    }
+}
