8f8d84e24625133c191da4bb801f355db1ae4543
francesco995 <francesco.gianni@live.it>
2017-05-09 16:22:17 +0200

    refactor

--- src/main/java/it/polimi/ingsw/ps09/client/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ps09/client/ClientApp.java
@@ -3,10 +3,9 @@ package it.polimi.ingsw.ps09.client;
 /**
  * Created by franc on 09/05/2017.
  */
-public class App
-{
+public class ClientApp {
     public static void main( String[] args )
     {
-        System.out.println( "Hello !" );
+        System.out.println( "Hello Client!" );
     }
 }
--- src/main/java/it/polimi/ingsw/ps09/server/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ps09/server/ServerApp.java
@@ -3,10 +3,9 @@ package it.polimi.ingsw.ps09.server;
 /**
  * Created by franc on 09/05/2017.
  */
-public class App
-{
+public class ServerApp {
     public static void main( String[] args )
     {
-        System.out.println( "Hello !" );
+        System.out.println( "Hello Server!" );
     }
 }
