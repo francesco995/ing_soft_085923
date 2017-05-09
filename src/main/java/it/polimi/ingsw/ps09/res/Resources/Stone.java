dda6b3978f8375be444f1c75111de64d55f97768
Francesco Lorenti <francesco1.lorenti@mail.polimi.it>
2017-05-09 18:20:03 +0200

    resources

---
+++ src/main/java/it/polimi/ingsw/ps09/res/Resources/Stone.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.ps09.res.Resources;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class Stone {
+    int mValue = 0;
+
+    public int getValue() {
+        return mValue;
+    }
+
+}
