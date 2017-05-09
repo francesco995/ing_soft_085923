b68d2b94c8b45176bccba41cc44c60037e0daae2
francesco995 <francesco.gianni@live.it>
2017-05-09 18:48:38 +0200

    Creade personal board

---
+++ src/main/java/it/polimi/ingsw/ps09/res/PersonalBoard.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.ps09.res;
+
+/**
+ * Created by franc on 09/05/2017.
+ */
+public class PersonalBoard {
+
+    private enum mSelectedBonusTile{
+        ONE, TWO;
+    }
+
+
+
+
+
+}
