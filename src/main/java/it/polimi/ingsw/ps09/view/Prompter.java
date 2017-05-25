505a654951d032004c7105a8a5be2dddc3736cd3
francesco995 <francesco.gianni@live.it>
2017-05-25 17:48:43 +0200

    add Prompter

---
+++ src/main/java/it/polimi/ingsw/ps09/view/Prompter.java
@@ -0,0 +1,56 @@
+package it.polimi.ingsw.ps09.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+/**
+ * Created by francesco995 on 25/05/2017.
+ */
+public class Prompter {
+
+    BufferedReader mBufferedStringReader = new BufferedReader(new InputStreamReader(System.in));
+    Scanner mReader = new Scanner(System.in);
+
+
+    public String promptForString(String message) throws IOException {
+        System.out.println(message);
+        String answer;
+
+        do {
+            answer = mReader.nextLine();
+
+        } while (answer.isEmpty());
+
+        return answer;
+    }
+
+    public int promptForIntChoice(String message, List<String> options){
+
+        int choice;
+
+        //First prints the message
+        System.out.println(message);
+
+        //Prints all the options available
+        printStringList(options);
+
+        //Asks for the choice
+        do {
+            choice = mReader.nextInt();
+        }while(choice < 0 && choice >= options.size());
+
+        return (choice - 1);
+    }
+
+    private void printStringList(List<String> options) {
+        IntStream.rangeClosed(1, options.size())
+                .mapToObj( i -> String.format("%d. %s", i, options.get( i - 1 ) ) )
+                .forEach(System.out::println);
+    }
+
+
+}
