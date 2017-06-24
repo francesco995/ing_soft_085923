235d407db7138a82cbddcabaa8c281f358d1f2f2
francesco995 <francesco.gianni@live.it>
2017-06-24 18:02:39 +0200

    ServerConnectionSocket: JavaDoc

--- src/main/java/it/polimi/ingsw/ps09/controller/Network/Client/ServerConnections/ServerConnectionSocket.java
+++ src/main/java/it/polimi/ingsw/ps09/controller/Network/Client/ServerConnections/ServerConnectionSocket.java
@@ -60,6 +60,12 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
     Gson mGson = null;
     GsonBuilder mGsonBuilder;
 
+    /**
+     * Socket Connection from player client to the server
+     * @param serverAddress Address of the Server to connect to
+     * @param userName UserName of the Player connecting to the Server
+     * @throws IOException
+     */
     public ServerConnectionSocket(String serverAddress, String userName) throws IOException {
 
         mSERVER_ADDRESS = InetAddress.getByName(serverAddress);
@@ -86,34 +92,67 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
 
     }
 
+    /**
+     * Check if the Player associated with this connection has an action available to do
+     * @return true if Player has Action(s) to do
+     */
     public boolean hasAction() {
         return mHasAction;
     }
 
+    /**
+     * Set HasAction parameter
+     * @param hasAction
+     */
     public void setHasAction(boolean hasAction){
         mHasAction = hasAction;
     }
 
+    /**
+     * Check if game is actually started
+     * @return true if game has started, false otherwise
+     */
     public boolean gameStarted(){
         return mGameStarted;
     }
 
+    /**
+     * Get latest version of Board
+     * @return Board received from server after las change
+     */
     public Board getBoard() {
         return mBoard;
     }
 
+    /**
+     * Get latest version of Players
+     * @return Players received from server after las change
+     */
     public HashMap<Integer, Player> getPlayers() {
         return mPlayers;
     }
 
+    /**
+     * Get latest version of PlayersOrder
+     * @return PlayersOrder received from server after las change
+     */
     public PlayersOrder getPlayersOrder() {
         return mPlayersOrder;
     }
 
+
+    /**
+     * Get the UserName of the player associated with this connection
+     * @return UserName string
+     */
     public String getUserName() {
         return mUserName;
     }
 
+    /**
+     * Get first message received
+     * @return First message on the received messages Queue
+     */
     public String getMessage() {
 
         while (!hasIncomingMessages()) {
@@ -122,10 +161,18 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
         return mIncomingMessages.poll();
     }
 
+    /**
+     * Get updated Actions list for player
+     * @return List of valid Actions for player
+     */
     public ArrayList<Action> getPlayerActionsList() {
         return mPlayerActionsList;
     }
 
+    /**
+     * Get all incoming messages not yet ridden
+     * @return List of received messages
+     */
     public List<String> getAllMessages() {
         List<String> messages = new LinkedList<>();
 
@@ -137,6 +184,10 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
     }
 
 
+    /**
+     * Check if connection has incoming messages
+     * @return Returns true if has new messages, false otherwise
+     */
     public boolean hasIncomingMessages() {
 
         if (!mIncomingMessages.isEmpty())
@@ -146,6 +197,10 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
     }
 
 
+    /**
+     * Sends a message via the socket output stream
+     * @param message Message to send
+     */
     public void sendMessage(String message) {
         //TODO: maybe switch to boolean return
         try {
@@ -160,6 +215,11 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
 
     }
 
+
+    /**
+     * Wait for a message, and deserialize updated Players
+     * it expects as many messages as there are players in PlayersOrder
+     */
     private void updatePlayers() {
 
         mPlayersOrder.getPlayersOrder().stream().forEach(
@@ -167,6 +227,10 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
 
     }
 
+    /**
+     * Wait for a message, and deserialize Actions
+     * first message is the number of actions to deserialize, next X messages are the serialized actions
+     */
     private void updateActions() {
 
         mPlayerActionsList = new ArrayList<>();
@@ -179,18 +243,28 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
 
     }
 
+    /**
+     * Wait for a message, and deserialize updated PlayersOrder
+     */
     private void updatePlayersOrder() {
 
         mPlayersOrder = mGson.fromJson(waitForMessage(), PlayersOrder.class);
 
     }
 
+    /**
+     * Wait for a message, and deserialize updated Board
+     */
     private void updateBoard() {
 
         mBoard = mGson.fromJson(waitForMessage(), Board.class);
 
     }
 
+    /**
+     * Sleep for x seconds unless Interrupted
+     * @param mS milliseconds to sleep for
+     */
     private void sleep(int mS) {
 
         try {
@@ -202,19 +276,35 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
     }
 
 
+    /**
+     * Check if socket is connected
+     * @return true if connected, false if not
+     */
     public boolean isConnected() {
         return mIsConnected;
     }
 
+    /**
+     * Return the port the socket is connected to
+     * @return Port as int
+     */
     public int getPort() {
         return mSocket.getPort();
     }
 
+    /**
+     * Return the address the socket is connected to
+     * @return Address as string
+     */
     public String getAddress() {
         return mSocket.getInetAddress().toString();
     }
 
 
+    /**
+     * Wait until input socket has a message, then return the message
+     * @return The received message
+     */
     private synchronized String waitForMessage() {
 
         String message;
@@ -234,6 +324,9 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
         return message;
     }
 
+    /**
+     * Wait for input socket to be ready with a complete message
+     */
     private void waitForSocketReady() {
         try {
             while (!mMessageReader.ready())
@@ -300,6 +393,7 @@ public class ServerConnectionSocket extends Thread implements ServerConnection {
                 }
             }
 
+            //When game start, the server will send a message to all players with their id to notify game has started
             mPlayerId = Integer.valueOf(waitForMessage());
 
             mGameStarted = true;
