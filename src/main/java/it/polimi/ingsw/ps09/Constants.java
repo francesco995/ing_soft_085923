15d87d8ee54531ae3ce42210295fdb14125e607f
francesco995 <francesco.gianni@live.it>
2017-06-30 11:04:58 +0200

    Constants class

---
+++ src/main/java/it/polimi/ingsw/ps09/Constants.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.ps09;
+
+/**
+ * Created by francesco995 on 30/06/2017.
+ * Application constants
+ */
+public final class Constants {
+
+
+    //####################################################
+    //####################################################
+    //############## Networking constants ################
+
+
+    /**
+     * Port for Socket connections
+     */
+    public static final int SOCKET_PORT = 1024;
+
+
+
+
+    //####################################################
+    //####################################################
+    //############## Game logic constants ################
+
+
+    /**
+     * Max players number for each game
+     */
+    public static final int MAX_PLAYERS = 4;
+
+
+
+
+
+    //####################################################
+    //####################################################
+    //############### Game view constants ################
+
+
+    /**
+     * Timeout for placement action
+     */
+    public static final int PLACEMENT_ACTION_TIMEOUT = 30;
+
+
+
+}
